898ceaa9efc0a46485eadd82b453a3030b10ae29
miguel <miguelanmartinez717@gmail.com>
2025-08-26 11:37:54 -0500

    area

--- backOrmPro/app/controller/areasController.ts
+++ backOrmPro/app/controller/areasController.ts
@@ -1,17 +1,51 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import AreaService from '#services/areaService'
+import { messages } from '@vinejs/vine/defaults'
 
 const areaService = new AreaService()
 
 export default class AreasController {
   async crearArea({ request, response }: HttpContext) {
-    const data = request.only(['id_tenant', 'nombre_area', 'codigo_area', 'descripcion'])
+    try{
+    const data = request.body()
     const area = await areaService.crearArea(data)
-    return response.status(201).json(area)
+    return response.json({ msj: 'Área creada', datos: area })
+    } catch (error) {
+      return response.json({ error: error.message, messages })
+    }
+
   }
 
   async listarAreas({ response }: HttpContext) {
     const areas = await areaService.listarAreas()
     return response.json(areas)
+  }
+
+  async listarId({ params, response }: HttpContext) {
+    try {
+      const area = await areaService.listarId(params.id)
+      return response.json({ msj: 'Área encontrada', datos: area })
+    } catch (error) {
+      return response.json({ error: error.message, messages })
+    }
+  }
+
+  async actualizar({ params, request, response }: HttpContext) {
+    try {
+      const data = request.body()
+      const area = await areaService.actualizar(params.id, data)
+      return response.json({ msj: 'Área actualizada', datos: area })
+    } catch (error) {
+      return response.json({ error: error.message, messages })
+    }
+  }
+
+  async eliminar({ params, response }: HttpContext) {
+    try {
+      const resp = await areaService.eliminar(params.id)
+      return response.json({ msj: resp })
+    } catch (error) {
+      return response.json({ error: error.message, messages })
+    }
   }
 }
--- backOrmPro/app/services/areaService.ts
+++ backOrmPro/app/services/areaService.ts
@@ -2,10 +2,38 @@ import Area from '../models/area.js'
 
 export default class AreaService {
   async crearArea(data: Partial<Area>) {
-    return await Area.create(data)
+    const area = await Area.create(data)
+    return area
   }
 
   async listarAreas() {
-    return await Area.query().preload('empresa'as any)
+    const areas = await Area.all()
+    return areas
+  }
+
+  async listarId(id: number) {
+    const area = await Area.find(id)
+    return area 
+  }
+
+  async actualizar(id: number, data: Partial<Area>) {
+    const area = await Area.find(id)
+    if (area) {
+      area.merge(data)
+      await area.save()
+      return area
+    } else {
+      return { error: 'Área no encontrada' }
+    }
+  }
+
+  async eliminar(id: number) {
+    const area = await Area.find(id)
+    if (area) {
+      await area.delete()
+      return 'Área eliminada'
+    } else {
+      return 'Área no encontrada'
+    }
   }
 }
